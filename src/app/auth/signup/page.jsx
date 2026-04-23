2fc0e8f81fd71626a4f07daa3b16f926bf38a375
MD-Sabbir-Hossain-Alif <sabbir.web.dp@gmail.com>
2026-04-24 02:12:26 +0600

    better auth configerd

---
+++ src/app/auth/signup/page.jsx
@@ -0,0 +1,112 @@
+"use client";
+import { authClient } from "@/lib/auth-client";
+import { Check } from "@gravity-ui/icons";
+import {
+    Button,
+    Description,
+    FieldError,
+    Form,
+    Input,
+    Label,
+    TextField,
+} from "@heroui/react";
+
+const SignUpPage = () => {
+    const onSubmit = async (e) => {
+        e.preventDefault();
+        const formData = new FormData(e.currentTarget);
+        const userData = Object.fromEntries(formData.entries());
+        console.log("Form submitted with:", userData);
+
+        const { data, error } = await authClient.signUp.email({
+            name: userData.name,
+            email: userData.email,
+            password: userData.password,
+        });
+        console.log("sign up response:", { data, error });
+    };
+    return (
+        <div className="container mx-auto">
+            <h2 className="text-4xl text-center my-10 underline">
+                Please Sign Up
+            </h2>
+
+            <Form
+                className="flex w-1/4 mx-auto flex-col gap-4 border p-4 rounded-2xl"
+                onSubmit={onSubmit}
+            >
+                <TextField
+                    isRequired
+                    name="name"
+                    type="text"
+                    validate={(value) => {
+                        if (value.length < 3) {
+                            return "Name must be at least 3 characters";
+                        }
+                        return null;
+                    }}
+                >
+                    <Label>Name</Label>
+                    <Input name="name" placeholder="John Doe" />
+                    <FieldError />
+                </TextField>
+                <TextField
+                    isRequired
+                    name="email"
+                    type="email"
+                    validate={(value) => {
+                        if (
+                            !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(
+                                value,
+                            )
+                        ) {
+                            return "Please enter a valid email address";
+                        }
+                        return null;
+                    }}
+                >
+                    <Label>Email</Label>
+                    <Input name="email" placeholder="john@example.com" />
+                    <FieldError />
+                </TextField>
+                <TextField
+                    isRequired
+                    minLength={8}
+                    name="password"
+                    type="password"
+                    validate={(value) => {
+                        if (value.length < 8) {
+                            return "Password must be at least 8 characters";
+                        }
+                        if (!/[A-Z]/.test(value)) {
+                            return "Password must contain at least one uppercase letter";
+                        }
+                        if (!/[0-9]/.test(value)) {
+                            return "Password must contain at least one number";
+                        }
+                        return null;
+                    }}
+                >
+                    <Label>Password</Label>
+                    <Input name="password" placeholder="Enter your password" />
+                    <Description>
+                        Must be at least 8 characters with 1 uppercase and 1
+                        number
+                    </Description>
+                    <FieldError />
+                </TextField>
+                <div className="flex gap-2">
+                    <Button type="submit">
+                        <Check />
+                        Submit
+                    </Button>
+                    <Button type="reset" variant="secondary">
+                        Reset
+                    </Button>
+                </div>
+            </Form>
+        </div>
+    );
+};
+
+export default SignUpPage;
